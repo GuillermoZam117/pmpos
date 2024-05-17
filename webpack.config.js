bb61ee1351c84140253db7ad04f43fc00eebb9bc
Guillermo Zambrano <guillermo.zambrano@arenso.com.mx>
2024-05-17 15:20:10 -0600

    actualizacion de errores en logica, index no carga

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
-    mode: 'development', // Change to 'production' for production build
-    entry: path.resolve(__dirname, 'app', 'index.jsx'),  // ensure the entry point is correct
+    mode: 'production',
+    entry: path.resolve(__dirname, 'app', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].[contenthash].js',
+        chunkFilename: '[name].[contenthash].js',
     },
     module: {
         rules: [
@@ -16,33 +18,37 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader'
-                }
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: ['@babel/plugin-transform-runtime'],
+                    },
+                },
             },
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            }
-        ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
     },
     plugins: [
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
+            filename: '[name].[contenthash].css',
         }),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'app', 'index.html'),  // ensure this path is correct
-            filename: 'index.html'
-        })
+            template: path.resolve(__dirname, 'app', 'index.html'),
+            filename: 'index.html',
+        }),
+        new BundleAnalyzerPlugin(),
     ],
     resolve: {
-        extensions: ['.js', '.jsx']  // handle .js and .jsx files
+        extensions: ['.js', '.jsx'],
     },
-    devServer: {
-        static: path.resolve(__dirname, 'public'),
-        compress: true,
-        port: 8080,
-        open: true,
-        hot: true
-    }
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+        runtimeChunk: 'single',
+    },
 };
